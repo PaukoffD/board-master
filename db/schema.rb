50a7b3b65086d3a05cb8d2b24a03c9641d68f36f
xuliaP <paukov2967@gmail.com>
2015-12-25 15:57:47 +0300

    actastree

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214085710) do
+ActiveRecord::Schema.define(version: 20151225103938) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 20151214085710) do
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
     t.integer  "category_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.integer  "parent_id"
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tmp_notices", force: :cascade do |t|
